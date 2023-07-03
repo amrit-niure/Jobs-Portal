b5082929603c2a913b6d7da6efd839f93eb1c4be
Amrit Niure <niureamrit2@gmail.com>
2023-07-03 13:44:18 +1000

    Starter Files ....

---
+++ client/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : { 
+        light : {
+            primary : '#861D88',
+            secondary : "#AEB0B4",
+            tertiary : '#F4F5F7'
+        },
+        dark : { 
+          // darkmode not configured
+        }
+      },
+      screens: {
+        'xsm': '320px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+  
+      },
+      fontFamily: {
+        myCustomFont: ['poppins', 'sans-serif'],
+      },
+      fontWeight :{
+        thin : '300',
+        regular : '400',
+        bold : '600',
+      }
+    },
+
+  },
+  plugins: [],
+}
+
---
+++ server/index.js
@@ -0,0 +1,67 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import multer from 'multer'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const app = express()
+dotenv.config()
+// Using  middlewares 
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(morgan("common"))
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}))
+app.use(bodyParser.json({limit : '30mb', extended : true}))
+app.use(bodyParser.urlencoded({limit : '30mb', extended : true}))
+
+// To get the file directory with its file name 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Images Storage Setup using multer 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/assets')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+// multer middleware to upload files
+const upload = multer({storage: storage});
+
+
+// Routes with multer upload 
+
+
+
+
+// Routes
+
+
+
+
+
+// Moongoose + server Setup
+const PORT = process.env.PORT || 8080
+const connectDB = async () => {
+    try {
+        // connection string 
+        const con = await mongoose.connect('mongodb://127.0.0.1:27017/jobsportal', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`Mongo DB Connected : ${con.connection.host}`)
+        app.listen(PORT,()=> console.log(`Server Listening on port : ${PORT}`))
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+// Establish a connection 
+connectDB()
